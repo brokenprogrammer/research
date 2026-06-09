72797f99f046e9b7775b7c21c6254e1c5419980d
Oskar Mendel <brokenprogrammer@gmail.com>
2026-06-09 22:36:28 +0200

    Allocation experiment.

--- Research.Console/Program.cs
+++ Research.Console/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var s = new PointStruct { X = 1, Y = 2 };
+
+object boxed = s;
+
+var c = new PointClass { X = 1, Y = 2 };
+
+Console.WriteLine("Hello World x2");
+
+GC.KeepAlive(boxed);
+GC.KeepAlive(c);
+
+struct PointStruct
+{
+    public int X;
+    public int Y;
+}
+
+class PointClass
+{
+    public int X;
+    public int Y;
+}
+
