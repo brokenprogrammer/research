155f4d70b30549cfe6fd4e093f57bc7d1508c94f
Oskar Mendel <brokenprogrammer@gmail.com>
2026-06-12 21:58:18 +0200

    Initial naive brc implementation

--- Research.Console/Program.cs
+++ Research.Console/Program.cs
@@ -1,68 +1,144 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Globalization;
 using System.Runtime.CompilerServices;
+using System.Text;
 using Research.Console.Memory;
-
-Console.WriteLine("Hello, World!");
+using Research.Shared._1BRC;
+using Research.Shared.Timing;
 
 Console.WriteLine($"PID: {Environment.ProcessId}");
-Console.WriteLine("Press Enter to start...");
-Console.ReadLine();
 
-SimpleMeasureable.Measure<MeasureStruct>("Struct");
-SimpleMeasureable.Measure<MeasureBoxedStruct>("Boxed Struct");
-SimpleMeasureable.Measure<MeasureClass>("Class");
-SimpleMeasureable.Measure<MeasureBoxedClass>("Boxed Class");
+Console.OutputEncoding = Encoding.UTF8;
 
-struct MeasureStruct : ISimpleMeasurable
+var stations = new Dictionary<string, Naive.ResultRow>();
+var naiveRun = new Naive("C:\\Users\\mende\\Documents\\Github\\research\\Data\\measurements-20.txt");
+using (new SimpleTimer())
 {
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public void Run()
-    {
-        var s = new PointStruct { X = 1, Y = 2 };
-    }
+    stations = naiveRun.Collect();
 }
 
-struct MeasureBoxedStruct : ISimpleMeasurable
-{
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public void Run()
-    {
-        var s = new PointStruct { X = 1, Y = 2 }; 
-        object o = s; 
-        GC.KeepAlive(o);
-    }
-}
+var measurements = new SortedDictionary<string, Naive.ResultRow>(stations);
+Console.WriteLine(
+    "{" + string.Join(", ", measurements.Select(kv => $"{kv.Key}={kv.Value}")) + "}");
 
-struct MeasureClass : ISimpleMeasurable
-{
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public void Run()
-    {
-        var c = new PointClass { X = 1, Y = 2 };
-    }
-}
+// var lines = File.ReadAllLines("C:\\Users\\mende\\Documents\\Github\\research\\Data\\measurements-20.txt", new UTF8Encoding());
+// foreach (var l in lines)
+// {
+//     var parts = l.Split(';');
+//     var name = parts[0];
+//     var value = Double.Parse(parts[1], CultureInfo.InvariantCulture);
+//     
+//     if (stations.TryGetValue(name, out var entry))
+//     {
+//         entry.Min = entry.Min < value ?  entry.Min : value; 
+//         entry.Max = entry.Max > value ?  entry.Max : value; 
+//         entry.Sum += value; 
+//         entry.Count = entry.Count + 1; 
+//     }
+//     else
+//     {
+//         stations[name] = new StationEntry()
+//         {
+//             Min = value,
+//             Max = value,
+//             Sum = value,
+//             Count = 1
+//         };
+//     }
+// }
 
-struct MeasureBoxedClass : ISimpleMeasurable
-{
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public void Run()
-    {
-        var c = new PointClass { X = 1, Y = 2 };
-        object o = c;
-        GC.KeepAlive(o);
-    }
-}
+// record ResultRow(double Min, double Mean, double Max)
+// {
+//     public override string ToString() =>
+//         $"{Round(Min)}/{Round(Mean)}/{Round(Max)}";
+//     
+//     private static double Round(double value) =>
+//         Math.Round(value * 10.0, MidpointRounding.AwayFromZero) / 10.0;
+// }
+//
+// class MeasurementAggregator
+// {
+//     public double Min = double.PositiveInfinity;
+//     public double Max = double.NegativeInfinity;
+//     public double Sum;
+//     public long Count;
+// }
 
-struct PointStruct
-{
-    public int X;
-    public int Y;
-}
+// var sortedStations = new SortedDictionary<string, StationEntry>(stations);
+//
+// var head = "{";
+// foreach (var kvp in sortedStations)
+// {
+//     var e = kvp.Value;
+//     var avg = e.Sum / e.Count;
+//     Console.Write($"{head}{kvp.Key}={e.Min:F1}/{avg:F1}/{e.Max:F1}");
+//     head = ", ";
+// }
+// Console.WriteLine("}");
 
-class PointClass
-{
-    public int X;
-    public int Y;
-}
+// public class StationEntry
+// {
+//     public double Min { get; set; } 
+//     public double Max { get; set; }
+//     public double Sum { get; set; }
+//     public int Count { get; set; }
+// }
 
+// SimpleMeasureable.Measure<MeasureStruct>("Struct");
+// SimpleMeasureable.Measure<MeasureBoxedStruct>("Boxed Struct");
+// SimpleMeasureable.Measure<MeasureClass>("Class");
+// SimpleMeasureable.Measure<MeasureBoxedClass>("Boxed Class");
+//
+// struct MeasureStruct : ISimpleMeasurable
+// {
+//     [MethodImpl(MethodImplOptions.NoInlining)]
+//     public void Run()
+//     {
+//         var s = new PointStruct { X = 1, Y = 2 };
+//     }
+// }
+//
+// struct MeasureBoxedStruct : ISimpleMeasurable
+// {
+//     [MethodImpl(MethodImplOptions.NoInlining)]
+//     public void Run()
+//     {
+//         var s = new PointStruct { X = 1, Y = 2 }; 
+//         object o = s; 
+//         GC.KeepAlive(o);
+//     }
+// }
+//
+// struct MeasureClass : ISimpleMeasurable
+// {
+//     [MethodImpl(MethodImplOptions.NoInlining)]
+//     public void Run()
+//     {
+//         var c = new PointClass { X = 1, Y = 2 };
+//     }
+// }
+//
+// struct MeasureBoxedClass : ISimpleMeasurable
+// {
+//     [MethodImpl(MethodImplOptions.NoInlining)]
+//     public void Run()
+//     {
+//         var c = new PointClass { X = 1, Y = 2 };
+//         object o = c;
+//         GC.KeepAlive(o);
+//     }
+// }
+//
+// struct PointStruct
+// {
+//     public int X;
+//     public int Y;
+// }
+//
+// class PointClass
+// {
+//     public int X;
+//     public int Y;
+// }
+//
---
+++ Research.Shared/1BRC/Naive.cs
@@ -0,0 +1,91 @@
+using System.Globalization;
+using System.Text;
+
+namespace Research.Shared._1BRC;
+
+public class Naive(string path)
+{
+    private string _path = path;
+
+    public Dictionary<string, ResultRow> Collect()
+    {
+        return
+            File.ReadLines(path, new UTF8Encoding())
+                .Select(l => l.Split(';'))
+                .GroupBy(parts => parts[0])
+                .ToDictionary(
+                    g => g.Key,
+                    g => g.Aggregate(
+                        new MeasurementAggregator(),
+                        (agg, parts) =>
+                        {
+                            var value = double.Parse(parts[1], CultureInfo.InvariantCulture);
+                            agg.Min = Math.Min(agg.Min, value);
+                            agg.Max = Math.Max(agg.Max, value);
+                            agg.Sum += value;
+                            agg.Count++;
+                            return agg;
+                        },
+                        agg => new ResultRow(
+                            agg.Min,
+                            (Math.Round(agg.Sum * 10.0, MidpointRounding.AwayFromZero) / 10.0) / agg.Count,
+                            agg.Max)));
+    }
+    
+    public record ResultRow(double Min, double Mean, double Max)
+    {
+        public override string ToString() =>
+            $"{Round(Min)}/{Round(Mean)}/{Round(Max)}";
+    
+        private static double Round(double value) =>
+            Math.Round(value * 10.0, MidpointRounding.AwayFromZero) / 10.0;
+    }
+
+    private class MeasurementAggregator
+    {
+        public double Min = double.PositiveInfinity;
+        public double Max = double.NegativeInfinity;
+        public double Sum;
+        public long Count;
+    }
+    
+    // public Dictionary<string, StationEntry> Perform()
+    // {
+    //     var stations = new Dictionary<string, StationEntry>();
+    //     var lines = File.ReadAllLines(_path, new UTF8Encoding());
+    //     foreach (var l in lines)
+    //     {
+    //         var parts = l.Split(';');
+    //         var name = parts[0];
+    //         var value = Double.Parse(parts[1], CultureInfo.InvariantCulture);
+    //
+    //         if (stations.TryGetValue(name, out var entry))
+    //         {
+    //             entry.Min = entry.Min < value ? entry.Min : value;
+    //             entry.Max = entry.Max > value ? entry.Max : value;
+    //             entry.Sum += value;
+    //             entry.Count = entry.Count + 1;
+    //         }
+    //         else
+    //         {
+    //             stations[name] = new StationEntry()
+    //             {
+    //                 Min = value,
+    //                 Max = value,
+    //                 Sum = value,
+    //                 Count = 1
+    //             };
+    //         }
+    //     }
+    //
+    //     return stations;
+    // }
+    
+    // public class StationEntry
+    // {
+    //     public double Min { get; set; } 
+    //     public double Max { get; set; }
+    //     public double Sum { get; set; }
+    //     public int Count { get; set; }
+    // }
+}
